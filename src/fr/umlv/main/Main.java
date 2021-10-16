5242ef66ba1e7ad0640a024017472c728c2975a9
FxTsuna <FxTsuna@outlook.fr>
2021-10-16 11:43:13 +0200

    Test de la creation de la table

--- src/fr/umlv/main/Main.java
+++ src/fr/umlv/main/Main.java
@@ -4,13 +4,23 @@ import org.apache.derby.jdbc.EmbeddedDataSource;
 
 import javax.naming.NamingException;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException, NamingException {
-        var dbUrl = "db";
-        var ds = new EmbeddedDataSource();
-        ds.setDatabaseName(dbUrl);
-        var conn = ds.getConnection();
-        System.out.println(conn.getClientInfo());
+        try {
+            var dbUrl = "db;create=true";
+            var ds = new EmbeddedDataSource();
+            ds.setDatabaseName(dbUrl);
+            var conn = ds.getConnection();
+            String sql = "Create Table calendar (id int not null generated always as identity," + "hours int, text varchar(128))";
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(sql);
+            System.out.println(conn.getClientInfo());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
