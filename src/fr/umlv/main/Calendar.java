6c53405344e614e6a505b0d8f21f882d34824182
FxTsuna <FxTsuna@outlook.fr>
2021-10-17 12:57:56 +0200

    Add dependency for @Entity

--- src/fr/umlv/main/Calendar.java
+++ src/fr/umlv/main/Calendar.java
@@ -1,5 +1,18 @@
 package fr.umlv.main;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Entity
 public class Calendar {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
