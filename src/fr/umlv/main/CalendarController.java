9754fd06760db03e4372ba0ff02579235b68a3d6
FxTsuna <FxTsuna@outlook.fr>
2021-10-23 10:15:14 +0200

    Starting get/post mapping

---
+++ src/fr/umlv/main/CalendarController.java
@@ -0,0 +1,11 @@
+package fr.umlv.main;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalendarController {
+
+    @GetMapping
+}
