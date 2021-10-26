10cf8ea3bfef78f44a514095e2f7c5ceda09cac9
Chan Mathieu <pro.matchi1@gmail.com>
2021-10-26 23:05:59 +0200

    src/fr.umlv.main: Update event services
    Added Add, remove and update services

--- src/fr/umlv/main/CalendarService.java
+++ src/fr/umlv/main/CalendarService.java
@@ -1,7 +1,24 @@
 package fr.umlv.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CalendarService {
+    @Autowired
+    private EventRepo eventRepository;
+
+    public void addEvent(Event event) {
+        eventRepository.save(event);
+    }
+
+    public void updateEvent(UUID uid, Event event) {
+        eventRepository.save(event);
+    }
+
+    public void removeEvent(UUID uid) {
+        eventRepository.deleteById(uid);
+    }
 }
