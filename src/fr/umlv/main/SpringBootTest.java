96089e56a56adab5abfc73ebe9bb75eb27aa424b
Chan Mathieu <pro.matchi1@gmail.com>
2021-10-28 23:02:31 +0200

    src/fr.umlv.main: Update app class

--- src/fr/umlv/main/SpringBootTest.java
+++ src/fr/umlv/main/SpringBootTest.java
@@ -4,15 +4,37 @@ import org.apache.derby.jdbc.EmbeddedDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class SpringBootTest {
-    public static void main(String[] args) throws SQLException {
+    public static String getQueries() throws IOException {
+        var queries = new StringBuilder();
+        var path = Path.of("create.sql");
+        Files.lines(path).forEach(queries::append);
+        return queries.toString();
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
         var dbUrl = "db/calendar";
         var ds = new EmbeddedDataSource();
+        SpringApplication.run(SpringBootTest.class, args);
         ds.setDatabaseName(dbUrl);
         ds.setCreateDatabase("create");
-        SpringApplication.run(SpringBootTest.class, args);
+        var conn = ds.getConnection();
+        var stmt = conn.createStatement();
+        var query1 = "CREATE SCHEMA hello";
+        stmt.execute(query1);
+        var query = "CREATE TABLE event( "
+                + "Id varchar(255) for bit data not null, "
+                + "Date VARCHAR(255), "
+                + "Heure VARCHAR(255), "
+                + "info VARCHAR(255), "
+                + "PRIMARY KEY (Id))";
+        stmt.execute(query);
+
     }
 }
