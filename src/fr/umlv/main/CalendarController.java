2434666a2910f8bcc649566a34883d0630dea3ef
Chan Mathieu <pro.matchi1@gmail.com>
2021-10-28 21:58:07 +0200

    src/fr.umlv.main: Update controller

--- src/fr/umlv/main/CalendarController.java
+++ src/fr/umlv/main/CalendarController.java
@@ -5,30 +5,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
 
-@Controller
+@RestController
 public class CalendarController {
     @Autowired
     private CalendarService calendarService;
     @Autowired
     private UserService userService;
 
+    /*
     @PostMapping("/calendar")
     public ResponseEntity<UserResponse> saveUser(@Validated UserSave input) {
         Objects.requireNonNull(input);
         var newUser = userService.addUser(input.username(), input.password());
         return new ResponseEntity<>(new UserResponse(newUser.getBody().id(), newUser.getBody().username()), HttpStatus.OK);
         //return ResponseEntity.created(new UserResponse(newUser.getBody().id(), newUser.getBody().username()));
-
     }
-
+    */
+    
     @GetMapping("/calendar")
-    public List<Event> getId() {
+    public List<Event> getEvent() {
         return calendarService.getEvents();
     }
+
+    @PostMapping("/calendar/put")
+    public void addEvent(@RequestBody Event event) {
+        System.out.println(event.toString());
+        calendarService.addEvent(event);
+    }
+
+    /*
+    @PutMapping("/put")
+    public ResponseEntity<UserResponse> updatePassword(@Validated UserSave input) {
+        var newPwd = userRepo.updatePassword(input.password());
+        return new ResponseEntity<UserSave>();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<UserResponse> deleteUser(@Validated UserSave input) {
+        userService.removeUser();
+    }
+    */
 }
+
