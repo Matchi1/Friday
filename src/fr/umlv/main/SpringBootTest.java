0c42391f6e8bc801bd2faa164abc585927bd54fd
Chan Mathieu <pro.matchi1@gmail.com>
2021-11-03 22:37:54 +0100

    src/fr.umlv.main/SpringBootTest: Remove unnecessary import

--- src/fr/umlv/main/SpringBootTest.java
+++ src/fr/umlv/main/SpringBootTest.java
@@ -1,16 +1,12 @@
 package fr.umlv.main;
 
-import org.apache.derby.jdbc.EmbeddedDataSource;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.SQLException;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class SpringBootTest {
     public static void main(String[] args) throws SQLException, IOException {
         SpringApplication.run(SpringBootTest.class, args);
