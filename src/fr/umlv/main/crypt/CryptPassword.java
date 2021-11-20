42dbd9fa05f1305892e8f37bcede54f14134350f
FxTsuna <FxTsuna@outlook.fr>
2021-11-20 11:33:43 +0100

    Crypting and decrypting password

--- src/fr/umlv/main/crypt/CryptPassword.java
+++ src/fr/umlv/main/crypt/CryptPassword.java
@@ -4,13 +4,15 @@ import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 public class CryptPassword {
+    private static final String encryptionKey = "CeSTfUNLeJAVAHa5";
+    private static final byte[] encryptionKeyBytes = encryptionKey.getBytes(); // Advanced Encryptiopn standard
+    private static final SecretKey secretKey = new SecretKeySpec(encryptionKeyBytes, "AES");
 
     public static byte[] cryptedPassword (String password) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException {
-        String encryptionKey = "CeSTfUNLeJAVAHa5";
-        byte[] encryptionKeyBytes = encryptionKey.getBytes(); // Advanced Encryptiopn standard
-        SecretKey secretKey = new SecretKeySpec(encryptionKeyBytes, "AES");
+        Objects.requireNonNull(password);
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         return cipher.doFinal(password.getBytes());
@@ -18,14 +20,11 @@ public class CryptPassword {
 
 
     public static String decryptedPassword (byte[] cryptedpassword) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        String encryptionKey = "CeSTfUNLeJAVAHa5";
-        byte[] encryptionKeyBytes = encryptionKey.getBytes();
-        SecretKey secretKey = new SecretKeySpec(encryptionKeyBytes, "AES");
+        Objects.requireNonNull(cryptedpassword);
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         var decryptedPassword = cipher.doFinal(cryptedpassword);
         return new String(decryptedPassword);
-
     }
 
 }
