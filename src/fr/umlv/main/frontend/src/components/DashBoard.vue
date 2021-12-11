880b090267630b9779d1ea0670e3468e9da41a14
Chan Mathieu <pro.matchi1@gmail.com>
2021-12-11 14:49:58 +0100

    frontend/src: Add components directory

---
+++ src/fr/umlv/main/frontend/src/components/DashBoard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'DashBoard',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    fetch("/event/all")
+        .then((response) => response.text())
+        .then((data) => {
+          this.msg = data;
+        });
+  }
+}
+</script>
