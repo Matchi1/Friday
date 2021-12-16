52b3a98ec913cecb14fd422e3830b01ce6b5b103
Chan Mathieu <pro.matchi1@gmail.com>
2021-12-16 15:05:39 +0100

    frontend: Add main application file

---
+++ src/fr/umlv/main/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Dashboard />
+</template>
+
+<script>
+import Dashboard from "@/components/DashBoard.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  }
+}
+</script>
+
+<style>
+
+</style>
