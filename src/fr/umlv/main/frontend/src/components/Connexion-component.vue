15675b7b67128b7b36d9abc260fcd1f9b103d835
FxTsuna <FxTsuna@outlook.fr>
2021-12-20 22:35:19 +0100

    css/html for the connexion page

--- src/fr/umlv/main/frontend/src/components/Connexion-component.vue
+++ src/fr/umlv/main/frontend/src/components/Connexion-component.vue
@@ -1,14 +1,25 @@
 <template>
-  <form>
-    <div>
-      <h1 class="font-bold text-green-500">Connexion</h1>
-      <label><b>Username</b></label>
-      <input type="text" placeholder="Enter your username here" v-model="username" required>
-      <label><b>Password</b></label>
-      <input type="password" placeholder="Enter your password here" v-model="password" required>
-      <input  type="submit" value="Sumbit">
-    </div>
-  </form>
+  <div class="mx-auto w-full max-w-xs">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" v-model="username" required>
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"  v-model="password" required>
+        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Sign In
+        </button>
+        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 
--- src/fr/umlv/main/frontend/src/views/Connexion-view.vue
+++ src/fr/umlv/main/frontend/src/views/Connexion-view.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>test</h1>
     <Connexion/>
   </div>
 </template>
