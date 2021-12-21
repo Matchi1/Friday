9647d864758ee5c8b9881ff465a7a7abd18b1287
FxTsuna <FxTsuna@outlook.fr>
2021-12-21 12:56:03 +0100

    Add an confirmation for password

--- src/fr/umlv/main/frontend/src/components/CreateUserComponent.vue
+++ src/fr/umlv/main/frontend/src/components/CreateUserComponent.vue
@@ -18,6 +18,12 @@
         <p class="text-gray-600 text-xs italic">Mettre une règle pour les mdp ici (peut-etre utf-8)</p>
       </div>
     </div>
+      <div class="mb-9">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="confirm">
+          Confirm your password
+        </label>
+        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="confirm" type="password" placeholder="Confirm your password"  v-model="confirm" required>
+      </div>
     <div>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:shadow-outline" type="button">
         Register
@@ -31,7 +37,8 @@ export default {
   name: "CreateUserComponent",
   data: () => ({
     username: '',
-    password: ''
+    password: '',
+    confirm:''
   })
 }
 </script>
