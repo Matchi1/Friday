58715776ef92f56646c68e11b30ce37314bacf7a
Chan Mathieu <pro.matchi1@gmail.com>
2021-12-25 15:12:49 +0100

    test/back: Add date and event formatter test

--- test/fr/umlv/main/back/event/EventTest.java
+++ test/fr/umlv/main/back/event/EventTest.java
@@ -1,7 +1,11 @@
 package fr.umlv.main.back.event;
 
+import fr.umlv.main.DateDetails;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class EventTest {
     @Test
@@ -14,4 +18,18 @@ public class EventTest {
         Assertions.assertThrows(NullPointerException.class, () -> new EventSaveDTO(null, null, null, ""));
     }
 
+    @Test
+    public void shouldReturnCorrectEventNoPeriodString() {
+        var start = new DateDetails(2000, 0, 1, 0, 0);
+        var event = Event.createEvent(new EventSaveDTO("hello", start, "no info"));
+        Assertions.assertEquals("hello:20000101T0000:no info", event.toString());
+    }
+
+    @Test
+    public void shouldReturnCorrectEventWithPeriodString() {
+        var start = new DateDetails(2000, 0, 1, 0, 0);
+        var end = new DateDetails(2001, 0, 1, 0, 0);
+        var event = Event.createEvent(new EventSaveDTO("hello", start, end, "no info"));
+        Assertions.assertEquals("hello:20000101T0000/20010101T0000:no info", event.toString());
+    }
 }
