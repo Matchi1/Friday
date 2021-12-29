0e844cea597b60a3ff861372b10bc08615d2ba3c
FxTsuna <FxTsuna@outlook.fr>
2021-12-29 22:50:23 +0100

    Deleting account and add a new button on dashboard for it

--- src/front/src/components/DashBoard.vue
+++ src/front/src/components/DashBoard.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div class="flex place-items-center">
+      <!-- buttons -->
+      <div class="text-center ml-auto mr-20">
+        <button class="p-3 pl-5 pr-5 bg-indigo-500 hover:bg-blue-400 text-gray-100 text-lg rounded-lg focus:border-4 border-indigo-300" @click="deleteAccount">Delete account</button>
+       </div>
+    </div>
   <!-- Next rdv -->
     <div class="flex-content max-w-screen-lg bg-purple-500 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4">
       <h2 class="text-3xl leading-9 font-bold tracking-tight text-white sm:text-4xl sm:leading-10">
@@ -17,7 +23,7 @@
     <!-- Liste rdv -->
     <div class="flex-content max-w-screen-lg bg-purple-500 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4">
       <h2 class="text-3xl leading-9 font-bold tracking-tight text-white sm:text-4xl sm:leading-10">
-        Jsp
+        Liste des rendez-vous du jour
       </h2>
       <div class="mt-8 flex justify-center">
         <div class="inline-flex rounded-md bg-white shadow">
@@ -31,12 +37,12 @@
     <!-- Calendar -->
     <div class="flex-content max-w-screen-lg bg-purple-500 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4">
       <h2 class="text-3xl leading-9 font-bold tracking-tight text-white sm:text-4xl sm:leading-10">
-        Jsp
+        Calendrier des rendez-vous du mois
       </h2>
       <div class="mt-8 flex justify-center">
         <div class="inline-flex rounded-md bg-blue-500 shadow">
           <a class="text-gray-200 font-bold py-2 px-6">
-            <router-link to="/fullcalendar">Calendrier du mois</router-link>
+            <router-link to="/fullcalendar">Vers le calendrier</router-link>
           </a>
         </div>
       </div>
@@ -45,6 +51,8 @@
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
   name: 'DashBoard',
 
@@ -59,6 +67,22 @@ export default {
     calendar() {
       const username = JSON.parse(localStorage.getItem('user'));
       console.log(username)
+    },
+
+    deleteAccount() {
+      const username = JSON.parse(localStorage.getItem('user'));
+      fetch("/user/delete/" + username,
+          {
+            method:'DELETE',
+            headers: {"Content-Type": "application/json"},
+          }).then((res => {
+            if (res.status === 200) {
+              router.push("Connexion")
+            } else {
+              console.log(username)
+            }
+          })
+      )
     }
   }
 
