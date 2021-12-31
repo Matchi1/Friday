c845be9f603cf03f35f503c22d9d521e69d6c1db
Chan Mathieu <pro.matchi1@gmail.com>
2021-12-31 10:47:21 +0100

    test/back: Add application sanity check

---
+++ src/test/java/fr/umlv/back/SpringBootAppTest.java
@@ -0,0 +1,14 @@
+package fr.umlv.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootAppTest {
+    /**
+     * Sanity check
+     */
+    @Test
+    public void contextLoads() {
+    }
+}
